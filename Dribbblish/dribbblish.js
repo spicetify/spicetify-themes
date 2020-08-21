577524bcc964e73f9abfb4da3f10d5db453d29d1
Julien Maille <julien.maille@gmail.com>
2020-08-21 23:16:03 +0200

    update main color using cover art "vibrant" color

--- Dribbblish/dribbblish.js
+++ Dribbblish/dribbblish.js
@@ -137,3 +137,109 @@ waitForElement([".LeftSidebar"], (queries) => {
     fade.id = "dribbblish-sidebar-fade-in";
     queries[0].append(fade);
 });
+
+function getAlbumInfo(uri) {
+    return new Promise((resolve) => { Spicetify.CosmosAPI.resolver.get(`hm://album/v1/album-app/album/${uri}/desktop`, (err, raw) => {
+        resolve(!err && raw.getJSONBody())
+    })})
+}
+
+var nearTrackSpan = null
+var nearArtistSpan = null
+var mainColor = getComputedStyle(document.documentElement).getPropertyValue('--modspotify_main_fg')
+var lastDoc = null
+
+waitForElement([".track"], (queries) => {
+    nearTrackSpan = document.createElement("span");
+    nearTrackSpan.id = "dribbblish-track-info";
+    queries[0].append(nearTrackSpan);
+});
+
+waitForElement([".artist"], (queries) => {
+    nearArtistSpan = document.createElement("span");
+    nearArtistSpan.id = "dribbblish-album-info";
+    queries[0].append(nearArtistSpan);
+});
+
+function hexToRgb(hex) {
+    var bigint = parseInt(hex.replace("#",""), 16);
+    var r = (bigint >> 16) & 255;
+    var g = (bigint >> 8) & 255;
+    var b = bigint & 255;
+    return r + "," + g + "," + b;
+}
+
+function LightenDarkenColor(col, amt) {
+  var num = parseInt(col, 16);
+  var r = (num >> 16) + amt;
+  var b = ((num >> 8) & 0x00FF) + amt;
+  var g = (num & 0x0000FF) + amt;
+  var newColor = g | (b << 8) | (r << 16);
+  return '#'+newColor.toString(16);
+}
+
+function updateColors(root) {
+    colHex = mainColor
+    colRGB = hexToRgb(colHex)
+    darkerColHex = LightenDarkenColor(colHex, -40)
+    darkerColRGB = hexToRgb(darkerColHex)
+
+    root.style.setProperty('--modspotify_main_fg', colHex)
+    root.style.setProperty('--modspotify_active_control_fg', colHex)
+    root.style.setProperty('--modspotify_secondary_bg', colHex)
+    root.style.setProperty('--modspotify_active_control_fg', colHex)
+    //root.style.setProperty('--modspotify_pressing_button_bg', colHex)
+    //root.style.setProperty('--modspotify_indicator_fg_and_button_bg', colHex)
+    root.style.setProperty('--modspotify_pressing_fg', colHex)
+    root.style.setProperty('--modspotify_sidebar_indicator_and_hover_button_bg', colHex)
+    //root.style.setProperty('--modspotify_scrollbar_fg_and_selected_row_bg', darkerColHex)
+    root.style.setProperty('--modspotify_selected_button', darkerColHex)
+    //root.style.setProperty('--modspotify_miscellaneous_hover_bg', colHex)
+    
+    root.style.setProperty('--modspotify_rgb_main_fg', colRGB)
+    root.style.setProperty('--modspotify_rgb_active_control_fgg', colRGB)
+    root.style.setProperty('--modspotify_rgb_secondary_bg', colRGB)
+    root.style.setProperty('--modspotify_rgb_active_control_fg', colRGB)
+    //root.style.setProperty('--modspotify_rgb_pressing_button_bg', colRGB)
+    //root.style.setProperty('--modspotify_rgb_indicator_fg_and_button_bg', colRGB)    
+    root.style.setProperty('--modspotify_rgb_pressing_fg', colRGB)
+    root.style.setProperty('--modspotify_rgb_sidebar_indicator_and_hover_button_bg', colRGB)
+    //root.style.setProperty('--modspotify_rgb_scrollbar_fg_and_selected_row_bg', darkerColRGB)
+    root.style.setProperty('--modspotify_rgb_selected_button', darkerColRGB)
+    //root.style.setProperty('--modspotify_rgb_miscellaneous_hover_bg', colRGB)
+}
+
+async function songchange() {
+    album_uri = Spicetify.Player.data.track.metadata.album_uri
+    
+    if (album_uri!==undefined) {
+        const albumInfo = await getAlbumInfo(album_uri.replace("spotify:album:", ""))
+
+        album_date = new Date(albumInfo.year, (albumInfo.month || 1)-1, albumInfo.day|| 0)
+        recent_date = new Date()
+        recent_date.setMonth(recent_date.getMonth() - 6)
+        album_date = album_date.toLocaleString('default', album_date>recent_date ? { year: 'numeric', month: 'short' } : { year: 'numeric' })
+        
+        if (nearTrackSpan!==null) nearTrackSpan.innerText = " • " + Spicetify.Player.data.track.metadata.popularity + "%"
+        if (nearArtistSpan!==null) nearArtistSpan.innerText = " • " + Spicetify.Player.data.track.metadata.album_title + " • " + album_date
+
+    } else {
+        // podcast?
+        nearTrackSpan.innerText = ""
+        nearArtistSpan.innerText = ""
+    }
+
+    Spicetify.colorExtractor(Spicetify.Player.data.track.uri)
+        .then((colors) => {
+            mainColor = colors['LIGHT_VIBRANT']
+            updateColors(document.documentElement) // main app
+            if (document.querySelector("#app-queue")!=null) updateColors(document.querySelector("#app-queue").contentDocument.documentElement) // now playing
+            if (lastDoc!==null) updateColors(lastDoc) // current iframe
+        })
+}
+
+Spicetify.Player.addEventListener("songchange", songchange)
+Spicetify.Player.addEventListener("appchange", ({"data": data}) => {
+    lastDoc = data.isEmbeddedApp ? data.container : data.container.contentDocument.documentElement
+    updateColors(lastDoc)
+})
