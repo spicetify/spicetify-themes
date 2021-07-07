51f36635889ec137fa45f534fece2c660c25809b
Grason Chan <grasonchan@gmail.com>
2021-07-08 00:40:08 +0800

    feat(Turntable): add context menu item "Enable blur backdrop" to toggle backdrop effect
    You can right click in the Full App Display to find it.

--- Turntable/rotateTurntable.js
+++ Turntable/rotateTurntable.js
@@ -7,14 +7,15 @@ window.addEventListener("load", rotateTurntable = () => {
   }
 
   const adModalStyle = document.createElement("style");
-  const STYLE_FOR_AD_MODAL = `
+  const STYLE_FOR_AD_MODAL =
+`
 .ReactModalPortal {
   display: none
 }
 `;
   adModalStyle.innerHTML = STYLE_FOR_AD_MODAL;
 
-  let playState;
+  let playState, clickedFadBtn;
 
   function handleRotate(fromEvent) {
     const fadArt = document.querySelector("#fad-art-image");
@@ -43,6 +44,8 @@ window.addEventListener("load", rotateTurntable = () => {
   function handleConfigSwitch() {
     const genericModal = document.querySelector("generic-modal");
 
+    document.querySelectorAll("#popup-config-container > div")[0].remove();
+
     handleInitalStatus(genericModal);
   }
 
@@ -53,12 +56,53 @@ window.addEventListener("load", rotateTurntable = () => {
     handleFadDblclick();
   }
 
+  function handleBackdrop() {
+    const fadClassList = document.querySelector("#full-app-display").classList;
+
+    if (!+localStorage.getItem("enableBlurFad")) {
+      fadClassList.add("blur-fad");
+
+      localStorage.setItem("enableBlurFad", "1");
+    } else {
+      fadClassList.remove("blur-fad");
+
+      localStorage.setItem("enableBlurFad", "0");
+    }
+  }
+
   function handleContextMenu() {
+    const configPopupCloseBtn = document.querySelector(".main-trackCreditsModal-closeBtn");
+    const configContainer = document.querySelector("#popup-config-container");
+    const settingRowReferenceNode = document.querySelectorAll("#popup-config-container > div")[0];
+
+    const settingRowContainer = document.createElement("div");
+    const settingRow =
+`
+<div class="setting-row">
+  <label class="col description">Enable blur backdrop</label>
+  <div class="col action">
+    <button class="${+localStorage.getItem("enableBlurFad") ? "switch" : "switch disabled"}">
+      <svg height="16" width="16" viewBox="0 0 16 16" fill="currentColor">
+        <path d="M13.985 2.383L5.127 12.754 1.388 8.375l-.658.77 4.397 5.149 9.618-11.262z"></path>
+      </svg>
+    </button>
+  </div>
+</div>
+`;
+
+    settingRowContainer.innerHTML = settingRow;
+    configContainer.insertBefore(settingRowContainer, settingRowReferenceNode);
+
+    const setBlurBackdropNode = document.querySelectorAll("#popup-config-container > div")[0];
     const configSwitchBtns = document.querySelectorAll("#popup-config-container button.switch");
+
+    configPopupCloseBtn.addEventListener("click", () => setBlurBackdropNode.remove());
 
     for (const configSwitch of configSwitchBtns) {
       configSwitch.addEventListener("click", handleConfigSwitch);
     }
+
+    setBlurBackdropNode.querySelector("button").addEventListener("click", handleBackdrop);
   }
 
   function handleMainInterface() {
@@ -88,6 +132,10 @@ window.addEventListener("load", rotateTurntable = () => {
     const topbarContentFadeIn = document.querySelector(".main-entityHeader-topbarContentFadeIn");
     const fullAppDisplay = document.querySelector("#full-app-display");
     const fadArt = document.querySelector("#fad-art-image");
+    const fadClassList = document.querySelector("#full-app-display").classList;
+
+    if (!clickedFadBtn && +localStorage.getItem("enableBlurFad")) fadClassList.add("blur-fad");
+    if (!clickedFadBtn) clickedFadBtn = true;
 
     playState
       ? fadArt.style.animationPlayState = "running"
