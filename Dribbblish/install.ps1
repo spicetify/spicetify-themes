4402d0c5c14e3ed7cd54f7fdd087dcf5a7082378
Klaus Kirnbauer <kirnbauer@users.noreply.github.com>
2021-07-25 14:35:57 +0200

    fix download path

--- Dribbblish/install.ps1
+++ Dribbblish/install.ps1
@@ -7,7 +7,7 @@ if ($null -eq $checkSpice) {
 
 Write-Host "Downloading themes package:" -ForegroundColor Green
 $zipFile = "$env:TEMP\spicetify-themes.zip"
-Invoke-WebRequest "https://github.com/morpheusthewhite/spicetify-themes/archive/refs/heads/v2.zip" -OutFile $zipFile
+Invoke-WebRequest "https://github.com/morpheusthewhite/spicetify-themes/archive/refs/heads/master.zip" -OutFile $zipFile
 
 Write-Host "Extracting themes package:" -ForegroundColor Green
 $extractPath = "$env:TEMP\spicetify-themes-2"
