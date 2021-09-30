986a4b22b25dd7ffdcc37d34f194994dad58697b
Davoleo <dav@davoleo.net>
2021-09-30 14:30:49 +0200

    Change album / year divider from — to  •
    - The long dash can create confusion on japanese albums that have a similar character in them (used in japanese to express long vocals) https://open.spotify.com/album/129TFxq98ubI8cZfetjs3i?si=261e69f3fca542f8
    - This also adds consistency as the same dot separator is also used in Friend Activity to separate song name from artist

--- dribbblish-dynamic.js
+++ dribbblish-dynamic.js
@@ -231,7 +231,7 @@ async function songchange() {
         album_date = album_date.toLocaleString('default', album_date>recent_date ? { year: 'numeric', month: 'short' } : { year: 'numeric' })
         album_link = "<a title=\""+Spicetify.Player.data.track.metadata.album_title+"\" href=\""+album_uri+"\" data-uri=\""+album_uri+"\" data-interaction-target=\"album-name\" class=\"tl-cell__content\">"+Spicetify.Player.data.track.metadata.album_title+"</a>"
         
-        nearArtistSpanText = album_link + " — " + album_date
+        nearArtistSpanText = album_link + " • " + album_date
     } else if (Spicetify.Player.data.track.uri.includes('spotify:episode')) {
         // podcast
         bgImage = bgImage.replace('spotify:image:', 'https://i.scdn.co/image/')
