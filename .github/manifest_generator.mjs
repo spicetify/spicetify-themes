05f0d1f87e113052cf6fc28bf81f7c7caffd5798
Afonso Jorge Ramos <afonsojorgeramos@gmail.com>
2022-05-23 16:36:50 +0100

    feat: creation of manifest generator script

---
+++ .github/manifest_generator.mjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env zx
+
+const output = (await $`ls -d */`).stdout.trim();
+
+cd('.github');
+
+const final_manifest = await Promise.all(output.split('\n').map(async (dir) => {
+    cd('../' + dir);
+    const theme_manifest = (await $`cat manifest.json`).stdout.trim();
+    return JSON.parse(theme_manifest);
+}));
+
+cd('..')
+
+await $`echo ${JSON.stringify(final_manifest, null, 2)} > manifest.json`
